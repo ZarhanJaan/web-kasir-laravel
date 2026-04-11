8b6004f22ad7b781745e1e592ac872c3a2d902bd
ZarhanJaan <zarhanatallah123@gmail.com>
2026-04-11 20:16:35 +0700

    tes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,48 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
 	<title>Login Toko Sembako</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{asset('login_template/images/icons/favicon.ico')}}"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="{{asset('login_template/images/icons/favicon.ico')}}" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login_template/vendor/bootstrap/css/bootstrap.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('login_template/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('login_template/fonts/iconic/css/material-design-iconic-font.min.css')}}">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="{{asset('login_template/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="{{asset('login_template/fonts/iconic/css/material-design-iconic-font.min.css')}}">
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login_template/vendor/animate/animate.css')}}">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login_template/vendor/css-hamburgers/hamburgers.min.css')}}">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login_template/vendor/animsition/css/animsition.min.css')}}">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login_template/vendor/select2/select2.min.css')}}">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{asset('login_template/vendor/daterangepicker/daterangepicker.css')}}">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="{{asset('login_template/vendor/daterangepicker/daterangepicker.css')}}">
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login_template/css/util.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('login_template/css/main.css')}}">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('{{asset('login_template/images/bg-01.jpg')}}')">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-				<form method="POST" action="{{ route('login') }}" class="login100-form validate-form" >
+				<form method="POST" action="{{ route('login') }}" class="login100-form validate-form">
 					@csrf
 					<span class="login100-form-title ">
-						{{ __('Login') }} Admin
+						{{ __('Login') }} Test
 					</span>
 					<span>
 						<center>
@@ -45,27 +50,31 @@
 						</center>
 					</span>
 					<br>
-					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+					<div class="wrap-input100 validate-input m-b-23" data-validate="Username is reauired">
 						<span class="label-input100">{{ __('E-Mail Address') }}</span>
-						<input id="email" placeholder="Type your email" type="email" class="input100 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+						<input id="email" placeholder="Type your email" type="email"
+							class="input100 form-control @error('email') is-invalid @enderror" name="email"
+							value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                	@error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                	@enderror
+						@error('email')
+							<span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+						@enderror
 						<span class="focus-input100" data-symbol="&#xf206;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<span class="label-input100">{{ __('Password') }}</span>
-						<input id="password" placeholder="Type your password" type="password" class="input100 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+						<input id="password" placeholder="Type your password" type="password"
+							class="input100 form-control @error('password') is-invalid @enderror" name="password"
+							required autocomplete="current-password">
 
-                                	@error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                	@enderror
+						@error('password')
+							<span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+						@enderror
 						<span class="focus-input100" data-symbol="&#xf190;"></span>
 					</div>
 					<br><br>
@@ -90,26 +99,27 @@
 			</div>
 		</div>
 	</div>
-	
+
 
 	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
+
+	<!--===============================================================================================-->
 	<script src="{{asset('login_template/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="{{asset('login_template/vendor/animsition/js/animsition.min.js')}}"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="{{asset('login_template/vendor/bootstrap/js/popper.js')}}"></script>
 	<script src="{{asset('login_template/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="{{asset('login_template/vendor/select2/select2.min.js')}}"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="{{asset('login_template/vendor/daterangepicker/moment.min.js')}}"></script>
 	<script src="{{asset('login_template/vendor/daterangepicker/daterangepicker.js')}}"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="{{asset('login_template/vendor/countdowntime/countdowntime.js')}}"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="{{asset('login_template/js/main.js')}}"></script>
 
 </body>
+
 </html>
