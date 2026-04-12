40a0b7838b14e34286a2529babf440239e9466e0
ZarhanJaan <zarhanatallah123@gmail.com>
2026-04-12 13:10:40 +0700

    fix side bar

---
+++ resources/views/manajemen_user.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="content-wrapper">
+    <div class="page-header">
+        <h3 class="page-title">
+            <span class="page-title-icon bg-gradient-primary text-white me-2">
+                <i class="mdi mdi-account-multiple"></i>
+            </span> Manajemen User
+        </h3>
+    </div>
+    <div class="row">
+        <div class="col-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body text-center py-5">
+                    <h4 class="text-muted">On development</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
